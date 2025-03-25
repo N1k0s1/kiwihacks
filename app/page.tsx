1b1ac0e32505e5b7f0b86513215d8dcd5ebdb4a4
Pak Kid <160797397+pakkid@users.noreply.github.com>
2025-03-25 15:12:06 +1300

    Update social media links and bump version to 0.24.3

--- app/page.tsx
+++ app/page.tsx
@@ -709,23 +709,23 @@ export default function Home() {
                   href="#"
                   className="h-10 w-10 rounded-full bg-[#0a2a0a] border border-[#2a5a2a] flex items-center justify-center hover:bg-[#2a5a2a] transition-colors"
                 >
-                  <span className="sr-only">Twitter</span>
-                  <svg
+                  <span className="sr-only">X</span>
+                  <svg 
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="#90ee90"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 </a>15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+                    strokeWidth="2" 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    className="lucide lucide-twitter">
+                      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/>
                   </svg>
                 </a>
                 <a
-                  href="#"
+                  href="https://instagram.com/kiwihacks"
                   className="h-10 w-10 rounded-full bg-[#0a2a0a] border border-[#2a5a2a] flex items-center justify-center hover:bg-[#2a5a2a] transition-colors"
                 >
                   <span className="sr-only">Instagram</span>
