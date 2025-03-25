e582d821a7e0cda100403e2f306601eda6c748fe
Niko <128133376+N1k0s1@users.noreply.github.com>
2025-03-25 21:46:22 +1300

    added images to the gallery

--- data/gallery-items.ts
+++ data/gallery-items.ts
@@ -33,6 +33,207 @@ export type GalleryItem = {
         event: "2023", // Name of event for filtering
         location: "Auckland, New Zealand", // location of event
       },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard1.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard1.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard2.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard2.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },      
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard3.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard3.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard4.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard4.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard5.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard5.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard6.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard6.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard7.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard7.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard8.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard8.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard9.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard9.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard10.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard10.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard11.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard11.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard12.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard12.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard13.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard13.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard14.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard14.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard15.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard15.pngheight=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard16.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard16.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard17.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard17.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard18.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard18.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard19.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard19.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+      { 
+        id: "scrapyard2025", // useless
+        type: "image", // Define the type of thing you want to show
+        thumbnail: "/gallery/scrapyard/scrapyard20.png?height=400&width=600", // small image to be displayed on the post
+        src: "/gallery/scrapyard/scrapyard20.png?height=800&width=1200", // main thing you want to show
+        title: "N/A", // Title to be shown on the psot
+        description: "NA", // Description to be shown on the post
+        event: "Scrapyard Auckland", // Name of event for filtering
+        location: "Auckland, New Zealand", // location of event
+      },
+ 
 ]
 
 // Helper function to get the most recent items
