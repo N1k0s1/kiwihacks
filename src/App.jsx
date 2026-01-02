45a1e1a094f149021958c128e189556878fc2ab4
Dekoder-py <kyle@codingcorner.dev>
2026-01-03 11:41:54 +1300

    monospace + desc

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ export default function App() {
       </nav>
       <h1 class="display">KiwiHacks</h1>
       <p>March 21-22. GridAKL.</p>
+
+      <div>
+        <p class="mono">
+          KiwiHacks is a 24 hour hackathon run by teenagers for teenagers - if
+          you're 18 or under, join us for this amazing opportunity to make new
+          friends and build a project together!
+          <br />
+          It’ll be an unforgettable weekend of coding, mentorship, and fun. All
+          skill levels welcome.
+        </p>
+      </div>
     </>
   );
 }
