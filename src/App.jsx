4bd2d51aa2d588d75d5688e82aa1e9b149470a89
Christie <24125@gdc.school.nz>
2026-01-06 10:34:58 +1300

    Fixed stuff lmk your thoughts (#1)
    * Fixed stuff lmk your thoughts
    
    * remove underline from button text
    
    ---------
    
    Co-authored-by: Dekoder-py <kyle@codingcorner.dev>

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,56 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <header>
       <nav class="navbar">
         <a href="/">Home</a>
+        <a href="#faq">FAQ</a>
         <a href="#about">About</a>
         <a href="#sponsor">Sponsors</a>
+        <a
+          class="sign-up-link"
+          id="small-button"
+          href="https://forms.hackclub.com/kiwihacks"
+          target="_blank"
+        >
+          <b>Sign up for KiwiHacks</b>
+        </a>
       </nav>
+
+      <header id="home">
+        <h1 class="display" id="title">
+          KiwiHacks
+        </h1>
+        <h2 class="mono" id="subtitle">
+          Auckland’s 24hr hackathon for teenage coders and innovators
+        </h2>
       </header>
 
-      <h1 class="display" id="title">
-        KiwiHacks
-      </h1>
-      <h2 class="mono">
-        Auckland’s 24hr hackathon for teenage coders and innovators
-      </h2>
-      <p>March 21-22. GridAKL.</p>
+      <section id="basicinfo">
+        <p class="info">Sat 21/Sun 22 March 2026</p>
+        <p class="info">GridAKL/John Lysaght Building</p>
+        <p class="info">75+ participants</p>
+      </section>
 
       <a
-        id="sign-up-link"
+        class="sign-up-link"
+        id="big-button"
         href="https://forms.hackclub.com/kiwihacks"
         target="_blank"
       >
         <b>Sign up for KiwiHacks!</b>
       </a>
 
-      <div>
+      <section id="about">
         <p class="mono">
           KiwiHacks is a 24 hour hackathon run by teenagers for teenagers - if
           you're 18 or under, join us for this amazing opportunity to make new
           friends and build a project together!
-          <br />
+        </p>
+        <p class="mono">
           It’ll be an unforgettable weekend of coding, mentorship, and fun. All
           skill levels welcome.
         </p>
-      </div>
+      </section>
     </>
   );
 }
