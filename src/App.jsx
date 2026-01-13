8a521fdc3d603d000f4b073e752c39f56076a12e
Christie <24125@gdc.school.nz>
2026-01-14 11:40:13 +1300

    Added FAQ answers

--- src/App.jsx
+++ src/App.jsx
@@ -76,81 +76,38 @@ export default function App() {
       <section id="faq">
         <h1 class="mono">FAQ</h1>
         <div class="faq-box">
-          <h3>What is KiwiHacks?</h3>
-          <p>
-            KiwiHacks is Auckland's premier hackathon for high school students
-            and makers 18 and under. It's organized by a small team of
-            like-minded teenagers, who are all passionate about technology and
-            innovation.
-          </p>
-        </div>
+          <h2 class="mono faq-question">When & where is KiwiHacks hosted?</h2>
+          <p class="mono faq-answer">KiwiHacks will be hosted in Auckland at the GridAKL / John Lysaght Startup Coworking space on the 21st until the 22nd of March 2026. The event will run for 24 hours, starting at 10am on Saturday, finishing at 10am on Sunday. You can either keep hacking overnight, or to go home and return in the morning!
 
-        <div class="faq-box">
-          <h3>When & where is KiwiHacks hosted?</h3>
-          <p>
-            KiwiHacks will be hosted in Auckland, New Zealand at the GridAKL /
-            John Lysaght Startup Coworking space on the 21st until the 22nd of
-            March 2026. The event will run for 24 hours, with the start being
-            10am Saturday, and the end being 10am Sunday. You're welcome to
-            choose to keep hacking overnight, or to go home! Contact us if this
-            is the case!
-          </p>
-        </div>
-
-        <div class="faq-box">
-          <h3>Who can participate in KiwiHacks?</h3>
-          <p>All high-school students (18 and under) are welcome to come!</p>
-        </div>
-
-        <div class="faq-box">
-          <h3>All this, for free?</h3>
-          <p>
-            Yep! Food, swag, and everything else is all included! Shoutout to
-            our sponsors!
-          </p>
-        </div>
-
-        <div class="faq-box">
-          <h3>What do I need?</h3>
-          <p>
-            Your laptop, chargers, toiletries, sleeping bag (if you plan to stay
-            overnight), and an open mind! If you plan to work on a hardware
-            project, bring the tools you'll need. We'll have limited tools, a 3d
-            printer & soldering station available.
-          </p>
-        </div>
-
-        <div class="faq-box">
-          <h3>I'm not good at coding. Can I still participate?</h3>
-          <p>
-            This hackathon is for creatives of all skill levels! We'll have
-            workshops for beginners on how to code the basics, and everyone will
-            finish with a project!.
-          </p>
-        </div>
-
-        <div class="faq-box">
-          <h3>What can I make at KiwiHacks?</h3>
-          <p>
-            Anything you can imagine! Games? Apps? Websites? Programming
-            languages? Hardware? You name it! We'll have a bunch of resources
-            and mentors to help you out.
           </p>
         </div>
         <div class="faq-box">
-          <h3>What if my parents are concerned?</h3>
-          <p>
-            We're here to help! Parents can reach out to us at
-            niko@kiwihacks.org for questions.
-          </p>
+          <h2 class="mono faq-question">Who can attend KiwiHacks?</h2>
+          <p class="mono faq-answer">All high-school students 18 and under are welcome at KiwiHacks!</p>
         </div>
-
         <div class="faq-box">
-          <h3>What if I have more questions?</h3>
-          <p>
-            Contact us! Feel free to reach out to us via email at
-            niko@kiwihacks.org.
-          </p>
+          <h2 class="mono faq-question">I'm not a good coder. Can I come?</h2>
+          <p class="mono faq-answer">Absolutely! KiwiHacks is for creatives of all skill levels! We'll have workshops for beginners on how to code the basics, and everyone will finish with a project!.</p>
+        </div>
+        <div class="faq-box">
+          <h2 class="mono faq-question">What should I bring to the hackathon?</h2>
+          <p class="mono faq-answer">Bring your laptop, charger,  toiletries and sleeping bag (if you plan to stay overnight), and an open mind! If you plan to work on a hardware project, bring the tools you'll need. We'll have limited tools, a 3D printer & soldering station available. We'll provide food, drinks, and snacks throughout the event.</p>
+        </div>
+        <div class="faq-box">
+          <h2 class="mono faq-question">All this, for free?</h2>
+          <p class="mono faq-answer">Yep! KiwiHacks (and swag!) is completely free thanks to our generous sponsors. Just sign up, turn up, and have fun!</p>
+        </div>
+        <div class="faq-box">
+          <h2 class="mono faq-question">What can I make at KiwiHacks?</h2>
+          <p class="mono faq-answer">Anything you want! You can build a website, app, game, robot, or anything else you can dream up. We'll have a bunch resources and mentors to help you out!</p>
+        </div>
+        <div class="faq-box">
+          <h2 class="mono faq-question">What if my parents are concerned?</h2>
+          <p class="mono faq-answer">We understand that parents want to ensure their children are safe. If your parents have any questions or concerns, we're here to help - please have them reach out to us at niko@kiwihacks.org!</p>
+        </div>
+        <div class="faq-box">
+          <h2 class="mono faq-question">What if I have more questions?</h2>
+          <p class="mono faq-answer">No worries! Contact us! Feel free to reach out to us via email at niko@kiwihacks.org.</p>
         </div>
       </section>
     </>
